cf9fc5a817501143fe58cb1b6099d7ac5a301f6c
reneSchm <rene.schmieding@outlook.com>
2022-12-26 15:32:15 +0100

    replace vector<PDMM> by PDM-Node and Model classes

--- cpp/examples/pdmm.cpp
+++ cpp/examples/pdmm.cpp
@@ -1,4 +1,5 @@
 #include "memilio/compartments/simulation.h"
+#include "memilio/config.h"
 #include "memilio/epidemiology/populations.h"
 #include "memilio/compartments/compartmentalmodel.h"
 #include "memilio/math/eigen.h"
@@ -9,12 +10,14 @@
 #include "memilio/utils/time_series.h"
 
 #include <algorithm>
+#include <array>
 #include <cstddef>
 #include <iostream>
 #include <limits>
 #include <list>
 #include <memory>
 #include <numeric>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -36,11 +39,12 @@ template <class Status>
 struct AdoptionRate {
     Status from; // i
     Status to; // j
-    double factor; // gammahat_{ij}^k
+    ScalarType factor; // gammahat_{ij}^k
     Order order; // first or second
+    size_t location; // k
 
     template <class Population>
-    constexpr double operator()(const Population& p) const
+    constexpr ScalarType operator()(const Population& p) const
     {
         if (order == Order::second) {
             return factor * p.array()[from] * p.array()[to] / p.array().sum();
@@ -64,12 +68,12 @@ struct AdoptionRates {
 template <class Status>
 struct TransitionRate {
     Status status; // i
-    int from; // k
-    int to; // l
-    double factor; // lambda_i^{kl}
+    size_t from; // k
+    size_t to; // l
+    ScalarType factor; // lambda_i^{kl}
 
     template <class Population>
-    constexpr double operator()(const Population& p) const
+    constexpr ScalarType operator()(const Population& p) const
     {
         return factor * p.array()[status];
     }
@@ -86,23 +90,21 @@ struct TransitionRates {
 };
 
 template <class Status>
-using Params = mio::ParameterSet<AdoptionRates<Status>>;
-
-template <class Status>
-class PDMM : public mio::CompartmentalModel<Status, mio::Populations<Status>, Params<Status>>
+class PDMNode
+    : public mio::CompartmentalModel<Status, mio::Populations<Status>, mio::ParameterSet<AdoptionRates<Status>>>
 {
-    using Base = mio::CompartmentalModel<Status, mio::Populations<Status>, Params<Status>>;
+    using Base = mio::CompartmentalModel<Status, mio::Populations<Status>, mio::ParameterSet<AdoptionRates<Status>>>;
 
 public:
-    PDMM()
+    PDMNode()
         : Base(typename Base::Populations({Status::Count}, 0.), typename Base::ParameterSet())
     {
     }
-    void get_derivatives(Eigen::Ref<const Eigen::VectorXd> x, Eigen::Ref<const Eigen::VectorXd>, double t,
+    void get_derivatives(Eigen::Ref<const Eigen::VectorXd> x, Eigen::Ref<const Eigen::VectorXd>, ScalarType t,
                          Eigen::Ref<Eigen::VectorXd> dxdt) const override
     {
-        auto& params = this->parameters;
-        for (auto& rate : params.template get<AdoptionRates<Status>>()) {
+        const auto& params = this->parameters;
+        for (const auto& rate : params.template get<AdoptionRates<Status>>()) {
             dxdt[rate.from] -= rate(x);
             dxdt[rate.to] += rate(x);
         }
@@ -110,19 +112,72 @@ public:
 };
 
 template <class Status>
-class mio::Simulation<std::vector<PDMM<Status>>>
+class PDMModel
 {
 public:
-    Simulation(const std::vector<PDMM<Status>>& models, double t0 = 0., double dt = 0.1)
-        : m_simulations()
-        , m_results(models.size() * Status::Count)
-        , m_result_iterator(Status::Count, 0)
-        , m_t0(t0)
-        , m_dt(dt)
+    PDMModel(const std::vector<std::array<ScalarType, Status::Count>>& populations,
+             const std::list<AdoptionRate<Status>>& adoption_rates,
+             const std::list<TransitionRate<Status>>& transition_rates)
+        : m_transition_rates(transition_rates.begin(), transition_rates.end())
+        , m_metapopulations(populations.size())
     {
-        m_simulations.reserve(models.size());
-        for (auto&& m : models) {
-            m_simulations.push_back(Simulation<PDMM<Status>>(m, t0, dt));
+        assert(max_location_index(adoption_rates, transition_rates) < populations.size());
+        for (const auto& adoption_rate : adoption_rates) {
+            m_metapopulations[adoption_rate.location].parameters.template get<AdoptionRates<Status>>().push_back(
+                adoption_rate);
+        }
+        for (size_t k = 0; k < populations.size(); k++) {
+            for (size_t i = 0; i < Status::Count; i++) {
+                m_metapopulations[k].populations.array()[i] = populations[k][i];
+            }
+        }
+    }
+
+    const std::vector<PDMNode<Status>>& get_metapopulations() const
+    {
+        return m_metapopulations;
+    }
+
+    const typename TransitionRates<Status>::Type& get_transition_rates() const
+    {
+        return m_transition_rates;
+    }
+
+    // TODO: (de-)serialize
+
+private:
+    static constexpr size_t max_location_index(const std::list<AdoptionRate<Status>>& ars,
+                                               const std::list<TransitionRate<Status>>& trs)
+    {
+        size_t max = 0;
+        for (auto& ar : ars) {
+            max = std::max(max, ar.location);
+        }
+        for (auto& tr : trs) {
+            max = std::max({max, tr.from, tr.to});
+        }
+        return max;
+    }
+
+    const typename TransitionRates<Status>::Type m_transition_rates;
+    std::vector<PDMNode<Status>> m_metapopulations;
+};
+
+template <class Status>
+class Simulation
+{
+public:
+    Simulation(const PDMModel<Status>& model, ScalarType t0 = 0., ScalarType dt = 0.1)
+        : m_dt(dt)
+        , m_t0(t0)
+        , m_model(model)
+        , m_result_iterator(Status::Count, 0)
+        , m_results(model.get_metapopulations().size() * Status::Count)
+        , m_simulations()
+    {
+        m_simulations.reserve(model.get_metapopulations().size());
+        for (const auto& m : model.get_metapopulations()) {
+            m_simulations.push_back(mio::Simulation<PDMNode<Status>>(m, t0, dt));
         }
         m_results.add_time_point(t0);
         for (size_t i = 0; i < m_simulations.size(); i++) {
@@ -130,18 +185,18 @@ public:
                 m_simulations[i].get_result().get_last_value();
         }
     }
-    Eigen::Ref<Eigen::VectorXd> advance(double tmax, double dt_max = std::numeric_limits<double>::max())
+    Eigen::Ref<Eigen::VectorXd> advance(ScalarType tmax, ScalarType dt_max = std::numeric_limits<ScalarType>::max())
     {
         // determine how long we wait until the next transition occurs
-        double waiting_time = draw_waiting_time();
-        double remaining_time;
+        ScalarType waiting_time = draw_waiting_time();
+        ScalarType remaining_time;
         // iterate time by increments of m_dt
         while (m_t0 < tmax) {
             m_dt           = std::min({m_dt, tmax - m_t0});
             remaining_time = m_dt; // xi * Delta-t
             // check if one or more transitions occur during this time step
             if (waiting_time < m_dt) { // (at least one) event occurs
-                std::vector<double> rates(transition_rates.size()); // lambda_m (for all m)
+                std::vector<ScalarType> rates(m_model.get_transition_rates().size()); // lambda_m (for all m)
                 // perform transition(s)
                 do {
                     m_t0 += waiting_time; // event time t**
@@ -150,11 +205,12 @@ public:
                         sim.advance(m_t0);
                     }
                     // compute current transition rates
-                    std::transform(transition_rates.begin(), transition_rates.end(), rates.begin(), [&](auto&& r) {
-                        // we should normalize here by dividing by the cumulative transition rate,
-                        // but the DiscreteDistribution below effectively does that for us
-                        return compute_rate(r);
-                    });
+                    std::transform(m_model.get_transition_rates().begin(), m_model.get_transition_rates().end(),
+                                   rates.begin(), [&](auto&& r) {
+                                       // we should normalize here by dividing by the cumulative transition rate,
+                                       // but the DiscreteDistribution below effectively does that for us
+                                       return compute_rate(r);
+                                   });
                     // draw transition event and execute it
                     int event = mio::DiscreteDistribution<int>::get_instance()(rates);
                     perform_transition(event);
@@ -181,34 +237,32 @@ public:
         return m_results.get_last_value();
     }
 
-    mio::TimeSeries<double>& get_result()
+    mio::TimeSeries<ScalarType>& get_result()
     {
         return m_results;
     }
 
-    const TimeSeries<ScalarType>& get_result() const
+    const mio::TimeSeries<ScalarType>& get_result() const
     {
         return m_results;
     }
 
-    mio::TimeSeries<double>& get_result(size_t location)
+    mio::TimeSeries<ScalarType>& get_result(size_t location)
     {
         return m_simulations[location].get_result();
     }
 
-    const mio::TimeSeries<double>& get_result(size_t location) const
+    const mio::TimeSeries<ScalarType>& get_result(size_t location) const
     {
         return m_simulations[location].get_result();
     }
-
-    typename TransitionRates<Status>::Type transition_rates;
 
 private:
     /// @brief perform the given transition event, moving a single "agent"
     void perform_transition(int event)
     {
         // get the rate corresponding to the event
-        const auto& r = transition_rates[event];
+        const auto& r = m_model.get_transition_rates()[event];
         // transitioning one person at the current time
         auto& value = m_simulations[r.from].get_result().get_last_value()[static_cast<Eigen::Index>(r.status)];
         value -= 1;
@@ -218,24 +272,24 @@ private:
         m_simulations[r.to].get_result().get_last_value()[static_cast<Eigen::Index>(r.status)] += 1;
     }
     /// @brief compute the current value of a transition rate
-    double compute_rate(const TransitionRate<Status>& r) const
+    ScalarType compute_rate(const TransitionRate<Status>& r) const
     {
         return r(m_simulations[r.from].get_result().get_last_value());
     }
     /// @brief draw time until the next event occurs
-    double draw_waiting_time()
+    ScalarType draw_waiting_time()
     {
         // compute the current cumulative transition rate
-        double ctr = 0; // Lambda
-        for (auto rate : transition_rates) {
+        ScalarType ctr = 0; // Lambda
+        for (auto rate : m_model.get_transition_rates()) {
             ctr += compute_rate(rate);
         }
         // draw the normalized waiting time
-        double nwt = mio::ExponentialDistribution<double>::get_instance()(1.0); // tau'
+        ScalarType nwt = mio::ExponentialDistribution<ScalarType>::get_instance()(1.0); // tau'
         return nwt / ctr;
     }
     /// @brief dynamically set m_dt
-    void update_dt(double dt_max)
+    void update_dt(ScalarType dt_max)
     {
         m_dt = dt_max;
         for (auto&& sim : m_simulations) {
@@ -283,7 +337,7 @@ private:
                 }
                 else {
                     // result.get_time(i) <= m_results.get_time(j) < result.get_time(i + 1);
-                    double t =
+                    ScalarType t =
                         (m_results.get_time(j) - result.get_time(i)) / (result.get_time(i + 1) - result.get_time(i));
                     assert(0 <= t && t < 1);
                     // assign value at the correct segment
@@ -297,10 +351,11 @@ private:
             //i++; // so that m_result_iterator has the index of result.end()
         }
     }
-    std::vector<mio::Simulation<PDMM<Status>>> m_simulations;
-    mio::TimeSeries<double> m_results;
+    ScalarType m_dt, m_t0;
+    const PDMModel<Status>& m_model;
     std::vector<Eigen::Index> m_result_iterator;
-    double m_t0, m_dt;
+    mio::TimeSeries<ScalarType> m_results;
+    std::vector<mio::Simulation<PDMNode<Status>>> m_simulations;
 };
 
 template <class SC>
@@ -323,65 +378,30 @@ void print_to_terminal(const mio::TimeSeries<SC>& results, const std::vector<std
 
 int main()
 {
-    using Model  = PDMM<InfectionState>;
+    using Model  = PDMNode<InfectionState>;
     using Status = Model::Compartments;
     /*** CONFIG ***/
-    const int n_subdomains = 2;
     mio::set_log_level(mio::LogLevel::warn);
+    ScalarType kappa = 0.001;
     /*** END CONFIG ***/
 
-    auto pop_size = Model::Compartments::Count;
-
-    // vector of locations k; list entries = {staus_from, status_to, gammahat, order}
-    std::vector<std::list<AdoptionRate<Status>>> adoption_rates(n_subdomains);
-    // vector {status, location_from, location_to, lambda}
-    TransitionRates<Status>::Type transition_rates;
-
-    /*** CONFIG ***/
-    adoption_rates[0].push_back({Status::S, Status::I, 0.3, Order::second});
-    adoption_rates[0].push_back({Status::I, Status::R, 0.1, Order::first});
-    adoption_rates[1].push_back({Status::S, Status::I, 1, Order::second});
-    adoption_rates[1].push_back({Status::I, Status::R, 0.08, Order::first});
-
-    double kappa     = 0.001;
-    transition_rates = {{Status::S, 0, 1, 0.5 * kappa},  {Status::I, 0, 1, 0.1 * kappa},
-                        {Status::R, 0, 1, 0.1 * kappa},  {Status::S, 1, 0, 0.1 * kappa},
-                        {Status::I, 1, 0, 0.02 * kappa}, {Status::R, 1, 0, 0.2 * kappa}};
-
-    std::vector<std::vector<double>> populations{{1900, 100, 0}, {2000, 0, 0}};
-    /*** END CONFIG ***/
-
-    std::vector<Model> local_models(n_subdomains);
-
-    for (int k = 0; k < n_subdomains; k++) {
-        local_models[k].parameters.get<AdoptionRates<Status>>().reserve(adoption_rates[k].size());
-        for (auto& r : adoption_rates[k]) {
-            local_models[k].parameters.get<AdoptionRates<Status>>().emplace_back(r);
-        }
-        for (int i = 0; i < pop_size; i++) {
-            local_models[k].populations.array()[i] = populations[k][i];
-        }
-    }
-
-    int i = 0;
-    /* for (auto& m : local_models) {
-        auto results = mio::simulate(0, 10, 0.1, m);
-        std::cout << "Model " << ++i << "/" << n_subdomains << "\n";
-        print_to_terminal(results, {"S", "I", "R"});
-    } */
-
-    auto sim             = mio::Simulation<std::vector<Model>>(local_models, 0, 0.1);
-    sim.transition_rates = transition_rates;
+    PDMModel<Status> pdmm({{950, 50, 0}, {1000, 0, 0}},
+                          {{Status::S, Status::I, 0.3, Order::second, 0},
+                           {Status::I, Status::R, 0.1, Order::first, 0},
+                           {Status::S, Status::I, 1, Order::second, 1},
+                           {Status::I, Status::R, 0.08, Order::first, 1}},
+                          {{Status::S, 0, 1, 0.5 * kappa},
+                           {Status::I, 0, 1, 0.1 * kappa},
+                           {Status::R, 0, 1, 0.1 * kappa},
+                           {Status::S, 1, 0, 0.1 * kappa},
+                           {Status::I, 1, 0, 0.02 * kappa},
+                           {Status::R, 1, 0, 0.2 * kappa}});
+    auto sim = Simulation<Status>(pdmm, 0, 0.1);
     sim.advance(100);
 
-    std::cout << "Global Model " << 1 << "/" << n_subdomains << "\n";
+    std::cout << "Global Model " << 1 << "/2\n";
     print_to_terminal(sim.get_result(), {"S", "I", "R", "S", "I", "R"});
-    std::cout << "Global Model " << 2 << "/" << n_subdomains << "\n";
-
-    /* for (i = 0; i < n_subdomains; i++) {
-        std::cout << "Global Model " << i + 1 << "/" << n_subdomains << "\n";
-        print_to_terminal(sim.get_result(i), {"S", "I", "R"});
-    } */
+    std::cout << "Global Model " << 2 << "/2\n";
 
     return 0;
 }
