fb6a8887e788edd6d8063f9950fbc4e8670ff2d4
Rene Schmieding <rene.schmieding@outlook.com>
2023-08-29 10:24:03 +0200

    correct time values in smm

--- cpp/models/mpm/smm.h
+++ cpp/models/mpm/smm.h
@@ -93,8 +93,8 @@ public:
             current_time += m_waiting_times[next_event];
             // regularily save current state in m_results
             if (current_time > last_result_time + m_dt) {
-                last_result_time += m_dt;
-                m_result.add_time_point(last_result_time);
+                last_result_time = current_time;
+                m_result.add_time_point(current_time);
                 // copy from the previous last value
                 m_result.get_last_value() = m_result[m_result.get_num_time_points() - 2];
             }
