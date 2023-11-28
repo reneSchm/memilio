d15083419fc6bdcbaaf4fba9bd1d50ae529d351a
reneSchm <49305466+reneSchm@users.noreply.github.com>
2023-11-28 10:57:17 +0100

    update to parallel sampling instead of averaging

--- cpp/hybrid_paper/transition_rate_fitting.cpp
+++ cpp/hybrid_paper/transition_rate_fitting.cpp
@@ -1,3 +1,9 @@
+#include "dlib/threads/async.h"
+#include "dlib/threads/auto_mutex_extension.h"
+#include "dlib/threads/parallel_for_extension.h"
+#include "dlib/threads/thread_pool_extension.h"
+#include "dlib/threads/thread_specific_data_extension.h"
+#include "dlib/threads/threads_kernel_2.h"
 #include "memilio/data/analyze_result.h"
 #include "models/mpm/potentials/potential_germany.h"
 #include "models/mpm/potentials/map_reader.h"
@@ -12,6 +18,8 @@
 
 #include <filesystem>
 #include <iostream>
+#include <unistd.h>
+#include <vector>
 
 #define TIME_TYPE std::chrono::high_resolution_clock::time_point
 #define TIME_NOW std::chrono::high_resolution_clock::now()
@@ -54,7 +62,6 @@ struct FittingFunctionSetup {
     using Model  = mio::mpm::ABM<GradientGermany<States>>;
     using Status = Model::Status;
 
-    const WeightedGradient::GradientMatrix& gradient; // reference to a WeightedGradient.gradient
     const Eigen::MatrixXi metaregions;
     Eigen::MatrixXd reference_commuters; // number of commuters between regions
     double t_max;
@@ -82,10 +89,9 @@ struct FittingFunctionSetup {
 
     // uses a weighted potential, loads metaregions and commuter data, prepares agents and reference values.
     // needed for the model setup and error calculation in single_run_mobility_error
-    explicit FittingFunctionSetup(WeightedGradient& wg, const std::string& metaregions_pgm_file,
+    explicit FittingFunctionSetup(const WeightedGradient& wg, const std::string& metaregions_pgm_file,
                                   const std::string& mobility_data_directory, const double t_max)
-        : gradient(wg.gradient)
-        , metaregions([&metaregions_pgm_file]() {
+        : metaregions([&metaregions_pgm_file]() {
             std::ifstream ifile(metaregions_pgm_file);
             if (!ifile.is_open()) { // write error and abort
                 mio::log(mio::LogLevel::critical, "Could not open metaregion file {}", metaregions_pgm_file);
@@ -131,13 +137,14 @@ struct FittingFunctionSetup {
 };
 
 // creates a model, runs it, and calculates the l2 error for transition rates
-double single_run_mobility_error(const FittingFunctionSetup& ffs, const std::vector<double>& sigma)
+double single_run_mobility_error(const FittingFunctionSetup& ffs, const WeightedGradient::GradientMatrix& gradient,
+                                 const std::vector<double>& sigma)
 {
     TIME_TYPE pre_run = TIME_NOW;
     using Model       = FittingFunctionSetup::Model;
 
     // create model
-    Model model(ffs.agents, {}, ffs.gradient, ffs.metaregions, {}, sigma);
+    Model model(ffs.agents, {}, gradient, ffs.metaregions, {}, sigma);
 
     // run simulation
     mio::Simulation<Model> sim(model, 0, 0.1);
@@ -173,65 +180,107 @@ double single_run_mobility_error(const FittingFunctionSetup& ffs, const std::vec
     return std::sqrt(l_2);
 }
 
-double max_error  = 0;
-double acc_error  = 0;
-size_t num_errors = 0;
-
-double average_run_mobility_error(const FittingFunctionSetup& ffs, const std::vector<double>& sigma, int num_runs)
+double average_run_mobility_error(const FittingFunctionSetup& ffs, const WeightedGradient& wg,
+                                  const std::vector<double>& sigma, int num_runs)
 {
     std::vector<double> errors(num_runs);
 #pragma omp parallel for num_threads(num_runs)
     for (int run = 0; run < num_runs; ++run) {
-        errors[run] = single_run_mobility_error(ffs, sigma);
+        errors[run] = single_run_mobility_error(ffs, wg.gradient, sigma);
     }
-    double avg = std::accumulate(errors.begin(), errors.end(), 0.0) / errors.size();
-    max_error  = std::max(max_error, avg);
-    acc_error += avg;
-    num_errors++;
-    return avg;
+    return std::accumulate(errors.begin(), errors.end(), 0.0) / errors.size();
 }
 
 int main()
 {
-    int num_runs = (omp_get_max_threads() > 1) ? omp_get_max_threads() : 3;
-    std::cout << "num_runs = " << num_runs << "\n";
+    // NOTE: Use DLIB_NUM_THREADS to set a maximum number of threads
+    const std::string path        = "../../../";
+    const double solver_epsilon   = 0.1;
+    const double tmax             = 20;
+    const auto total_fitting_time = std::chrono::hours(48);
+    const double weight_min = 0, weight_max = 1000;
+    const double sigma_min = 0, sigma_max = 50;
+    const double slope_min = 0, slope_max = 10;
 
-    std::cout << "Current path is " << fs::current_path() << '\n';
+    double max_error  = 0;
+    double acc_error  = 0;
+    size_t num_errors = 0;
+    dlib::mutex print_mutex; // used to prevent parallel prints from bleeding into each other
+    // see also http://dlib.net/api.html#threads or http://dlib.net/threads_ex.cpp.html
+
+#ifdef USE_MICROSTEPPING
+    std::cout << "Microstepping:           Enabled\n";
+#else
+    std::cout << "Microstepping:           Disabled\n";
+#endif
+#ifdef USE_POPULATION_CHANGE_ERROR
+    std::cout << "Population change error: Enabled\n";
+#else
+    std::cout << "Population change error: Disabled\n";
+#endif
+    std::cout << "Current path is          " << fs::current_path() << "\n"
+              << "ABM tmax:                " << tmax << "\n"
+              << "Total threads:           " << dlib::default_thread_pool().num_threads_in_pool() << "\n"
+              << "Total fitting time:      " << PRINTABLE_TIME(total_fitting_time) << "\n"
+              << "Solver epsilon:          " << solver_epsilon << "\n"
+              << "\n";
 
     TIME_TYPE timer = TIME_NOW;
 
-    std::string path = "../../";
-
-    WeightedGradient wg(path + "potentially_germany_grad.json", path + "boundary_ids.pgm");
+    const WeightedGradient wg(path + "potentially_germany_grad.json", path + "boundary_ids.pgm");
+    std::vector<WeightedGradient::GradientMatrix> thread_specific_gradient(
+        dlib::default_thread_pool().num_threads_in_pool(), wg.base_gradient);
     restart_timer(timer, "# Time for creating weighted potential");
 
-    FittingFunctionSetup ffs(wg, path + "metagermany.pgm", path + "data/mobility/", 10);
+    const FittingFunctionSetup ffs(wg, path + "metagermany.pgm", path + "data/mobility/", tmax);
     restart_timer(timer, "# Time for fitting function setup");
 
+    const Eigen::Vector2d centre = {wg.gradient.rows() / 2.0, wg.gradient.cols() / 2.0}; // centre of the wg.gradient
+
     auto result = dlib::find_min_global(
+        dlib::default_thread_pool(), // use DLIB_NUM_THREADS (or nproc) threads
         [&](auto&& w1, auto&& w2, auto&& w3, auto&& w4, auto&& w5, auto&& w6, auto&& w7, auto&& w8, auto&& w9,
             auto&& w10, auto&& w11, auto&& w12, auto&& w13, auto&& w14, auto&& sigma1, auto&& sigma2, auto&& sigma3,
-            auto&& sigma4, auto&& sigma5, auto&& sigma6, auto&& sigma7, auto&& sigma8) {
-            // let dlib set the weights for the potential
-            wg.apply_weights({w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14});
-// calculate the transition rate error
-#pragma omp single
-            std::cerr << "W: " << w1 << ", " << w2 << ", " << w3 << ", " << w4 << ", " << w5 << ", " << w6 << ", " << w7
-                      << ", " << w8 << ", " << w9 << ", " << w10 << ", " << w11 << ", " << w12 << ", " << w13 << ", "
-                      << w14 << "\n";
-            std::cerr << "S: " << sigma1 << ", " << sigma2 << ", " << sigma3 << ", " << sigma4 << ", " << sigma5 << ", "
-                      << sigma6 << ", " << sigma7 << ", " << sigma8 << "\n";
-
-            auto err = average_run_mobility_error(ffs, {sigma1, sigma2, sigma3, sigma4, sigma5, sigma6, sigma7, sigma8},
-                                                  num_runs);
-
+            auto&& sigma4, auto&& sigma5, auto&& sigma6, auto&& sigma7, auto&& sigma8, auto&& slope) {
+            // set the weights for the potential
+            auto& gradient = thread_specific_gradient[dlib::get_thread_id()];
+            wg.apply_weights({w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14}, gradient);
+            // set an interior slope
+            for (Eigen::Index i = 0; i < gradient.rows(); i++) {
+                for (Eigen::Index j = 0; j < gradient.cols(); j++) {
+                    if (wg.base_gradient(i, j) == Eigen::Vector2d{0, 0}) {
+                        auto direction = (Eigen::Vector2d{i, j} - centre).normalized();
+                        gradient(i, j) = slope * direction;
+                    }
+                }
+            }
+            // calculate the transition rate error
+            auto err = single_run_mobility_error(ffs, gradient,
+                                                 {sigma1, sigma2, sigma3, sigma4, sigma5, sigma6, sigma7, sigma8});
+            // print out the used config and calculated error to std::err
+            dlib::auto_mutex lock_printing_until_end_of_scope(print_mutex); // locks on ctor, unlocks on destructor
+            std::cerr << " -w [" << w1 << ", " << w2 << ", " << w3 << ", " << w4 << ", " << w5 << ", " << w6 << ", "
+                      << w7 << ", " << w8 << ", " << w9 << ", " << w10 << ", " << w11 << ", " << w12 << ", " << w13
+                      << ", " << w14 << "]";
+            std::cerr << " -s [" << sigma1 << ", " << sigma2 << ", " << sigma3 << ", " << sigma4 << ", " << sigma5
+                      << ", " << sigma6 << ", " << sigma7 << ", " << sigma8 << "]";
+            std::cerr << " -sl " << slope << "\n";
             std::cerr << "E: " << err << "\n\n";
+            // gather some statistics
+            max_error = std::max(max_error, err);
+            acc_error += err;
+            num_errors++;
+            // return the error
             return err;
         },
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, // lower bounds
-        {1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
-         1000, 1000, 1000, 20,   20,   20,   20,   20,   20,   20,   20}, // upper bounds
-        std::chrono::hours(48) // run this long
+        {weight_min, weight_min, weight_min, weight_min, weight_min, weight_min, weight_min, weight_min,
+         weight_min, weight_min, weight_min, weight_min, weight_min, weight_min, sigma_min,  sigma_min,
+         sigma_min,  sigma_min,  sigma_min,  sigma_min,  sigma_min,  sigma_min,  slope_min}, // lower bounds
+        {weight_max, weight_max, weight_max, weight_max, weight_max, weight_max, weight_max, weight_max,
+         weight_max, weight_max, weight_max, weight_max, weight_max, weight_max, sigma_max,  sigma_max,
+         sigma_max,  sigma_max,  sigma_max,  sigma_max,  sigma_max,  sigma_max,  slope_max}, // upper bounds
+        total_fitting_time, // run this long
+        solver_epsilon // target accuracy for searching local minima, before returning to global search
     );
 
     restart_timer(timer, "# Time for minimizing");
