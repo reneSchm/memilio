76230253dd3183cee063f016b91652dcda3c069b
reneSchm <rene.schmieding@outlook.com>
2024-02-16 14:40:50 +0100

    use model setup class

--- cpp/hybrid_paper/estimate_transition_rates.cpp
+++ cpp/hybrid_paper/estimate_transition_rates.cpp
@@ -1,3 +1,5 @@
+#include "hybrid_paper/library/model_setup.h"
+#include "hybrid_paper/library/potentials/commuting_potential.h"
 #include "hybrid_paper/library/weighted_gradient.h"
 #include "hybrid_paper/library/initialization.h"
 #include "hybrid_paper/library/map_reader.h"
@@ -158,99 +160,24 @@ std::string colorize(double a, double b)
 
 int main(int argc, char** argv)
 {
-    using Model  = mio::mpm::ABM<CommutingPotential<StochastiK, mio::mpm::paper::InfectionState>>;
+    using Model  = mio::mpm::ABM<CommutingPotential<Kedaechtnislos, mio::mpm::paper::InfectionState>>;
     using Status = mio::mpm::paper::InfectionState;
-    // auto cli_result = mio::command_line_interface<Tmax>(argv[0], argc, argv);
-    // if (!cli_result) {
-    //     std::cerr << cli_result.error().formatted_message() << "\n";
-    //     return cli_result.error().code().value();
-    // }
-    auto tmax = 100; //cli_result.value().get<Tmax>();
 
-    Eigen::MatrixXi metaregions;
-
-    WeightedGradient wg(mio::base_dir() + "potentially_germany_grad.json", mio::base_dir() + "boundary_ids.pgm");
-
-    const std::vector<int> county_ids   = {233, 228, 242, 223, 238, 232, 231, 229};
-    Eigen::MatrixXd reference_commuters = get_transition_matrix(mio::base_dir() + "data/mobility/").value();
-    Eigen::MatrixXd reference_transitions_daily =
-        get_transition_matrix_daily_total(mio::base_dir() + "data/mobility/").value();
-    auto ref_pops = std::vector<double>{218579, 155449, 136747, 1487708, 349837, 181144, 139622, 144562};
-    auto ref_pop  = std::accumulate(ref_pops.begin(), ref_pops.end(), 0.0);
-
-    int num_regions = county_ids.size();
-    int num_weights = 14;
-
-    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> commute_weights(num_regions, num_regions);
-    commute_weights.setZero();
-    // for (int i = 0; i < num_regions; i++) {
-    //     commute_weights(i, i) = 1;
-    // }
-    for (int i = 0; i < num_regions; i++) {
-        for (int j = 0; j < num_regions; j++) {
-            if (i != j) {
-                commute_weights(i, j) = reference_commuters(
-                    county_ids[i],
-                    county_ids[j]); //commute_weights(i,j)/persons_per_agents + (commute_weights(j,i)/ref_pop)
-            }
-        }
-        commute_weights(i, i) = ref_pops[i] - commute_weights.row(i).sum();
-    }
-
-    std::cerr << "Setup: Read metaregions.\n" << std::flush;
-    mio::log_thread_local_rng_seeds(mio::LogLevel::info);
-    {
-        const auto fname = mio::base_dir() + "metagermany.pgm";
-        std::ifstream ifile(fname);
-        if (!ifile.is_open()) {
-            mio::log(mio::LogLevel::critical, "Could not open file {}", fname);
-            return 1;
-        }
-        else {
-            metaregions = mio::mpm::read_pgm_raw(ifile).first;
-            ifile.close();
-        }
-    }
-
-    auto sigmas  = std::vector<double>(num_regions, 10);
-    auto weights = std::vector<double>(14, 500);
-
-    wg.apply_weights(weights);
-
-    StochastiK k_provider(commute_weights, metaregions, {metaregions});
-    std::cerr << k_provider.metaregion_commute_weights << "\n";
-
-    std::string init_file = "/group/HPC/Gruppen/PSS/Modelle/Hybrid Models/Papers, Theses, "
-                            "Posters/2023_Paper_Spatio-temporal_hybrid/initializations/initSusceptible9375.json";
-
-    std::vector<Model::Agent> agents;
-
-    // std::vector<Eigen::Vector2d> positions{{592, 648}, {840, 604},  {896, 700}, {426, 136},  {626, 568},
-    //                                        {312, 688}, {880, 1166}, {682, 776}, {1088, 864}, {950, 406}};
-    // for (auto p : positions) {
-    //     agents.push_back({p, Status::S, int(metaregions(p[0], p[1]) - 1)});
-    // }
-
-    read_initialization(init_file, agents);
-    size_t num_agents        = agents.size();
-    double persons_per_agent = ref_pop / num_agents;
-
-    std::cout << "persons per agent: " << persons_per_agent << "\n";
-
-    Model model(k_provider, agents, {}, wg.gradient, metaregions, {Status::D}, sigmas);
+    mio::mpm::paper::ModelSetup<Model::Agent> setup;
+    Model model = setup.create_abm<Model>();
 
     //print_movement<Model>(agents, model, tmax, 0.1);
 
-    auto rates = calculate_transition_rates(model, 10, tmax, metaregions.maxCoeff(), ref_pops);
+    auto rates = calculate_transition_rates(model, 10, setup.tmax, setup.region_ids.size(), setup.populations);
 
     //check
     for (auto rate : rates) {
         //rates
         std::cout << rate.from << "->" << rate.to << ": rel: "
                   << colorize(rate.factor,
-                              (commute_weights(static_cast<size_t>(rate.from), static_cast<size_t>(rate.to)) +
-                               commute_weights(static_cast<size_t>(rate.to), static_cast<size_t>(rate.from))) /
-                                  ref_pops[rate.from.get()])
+                              (setup.commute_weights(static_cast<size_t>(rate.from), static_cast<size_t>(rate.to)) +
+                               setup.commute_weights(static_cast<size_t>(rate.to), static_cast<size_t>(rate.from))) /
+                                  setup.populations[rate.from.get()])
                   << std::endl;
     }
 
